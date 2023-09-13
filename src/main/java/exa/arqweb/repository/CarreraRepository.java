8fc0298fc921f2d26e6dca5336fe724007b89621
pedrochatelain <pedrochatelain@gmail.com>
2023-09-13 20:33:01 -0300

    add busqueda de carreras que tienen inscriptos

--- src/main/java/exa/arqweb/repository/CarreraRepository.java
+++ src/main/java/exa/arqweb/repository/CarreraRepository.java
@@ -1,8 +1,11 @@
 package exa.arqweb.repository;
 
+import java.util.List;
+
 import exa.arqweb.entity.Carrera;
 import exa.arqweb.repository.interfaces.Repository;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
 
 public class CarreraRepository implements Repository<Carrera> {
 
@@ -24,5 +27,15 @@ public class CarreraRepository implements Repository<Carrera> {
         
     }
 
+    // f) recuperar las carreras con estudiantes inscriptos, y ordenar por cantidad de inscriptos.
+    public List<Carrera> getCarrerasConInscriptos() {
+        String statement = """
+            SELECT c
+            FROM Carrera c JOIN Inscripcion i ON c.id = i.carrera.id
+            GROUP BY c.id
+        """;
+        Query query = em.createQuery(statement);
+        return query.getResultList();
+    }
     
 }
