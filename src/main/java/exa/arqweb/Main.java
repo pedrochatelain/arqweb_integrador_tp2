68a5b1f981ba4670dab68358b91d59d429657623
pedrochatelain <pedrochatelain@gmail.com>
2023-09-13 15:32:06 -0300

    agrega estudiante a db

--- src/main/java/exa/arqweb/Main.java
+++ src/main/java/exa/arqweb/Main.java
@@ -1,6 +1,7 @@
 package exa.arqweb;
 
 import exa.arqweb.entity.Carrera;
+import exa.arqweb.entity.Estudiante;
 import exa.arqweb.repository.interfaces.FactoryRepository;
 import exa.arqweb.repository.interfaces.Repository;
 
@@ -8,9 +9,23 @@ public class Main {
 
     public static void main( String[] args ) {
 
-        Repository<Carrera> repositoryCarrera = FactoryRepository.mysql().getRepositoryCarrera();
+        // Repository<Carrera> repositoryCarrera = FactoryRepository.mysql().getRepositoryCarrera();
+        // repositoryCarrera.add(new Carrera("lapendejada"));
+
+        // Creo estudiante y lo agrego a la db
+        Repository<Estudiante> repositoryEstudiante = FactoryRepository.mysql().getRepositoryEstudiante();
+        Estudiante e = new Estudiante(
+            "John", 
+            "Doe", 
+            40, 
+            "Male", 
+            2020202, 
+            "London", 
+            3993,
+            true
+        );
         
-        repositoryCarrera.add(new Carrera("random career"));
+        repositoryEstudiante.add(e);
 
     }
 
