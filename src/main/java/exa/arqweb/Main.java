2ec2610783a8e1c9fb743d85921042588fecb16a
pedrochatelain <pedrochatelain@gmail.com>
2023-09-13 11:19:33 -0300

    first commit

---
+++ src/main/java/exa/arqweb/Main.java
@@ -0,0 +1,13 @@
+package exa.arqweb;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
