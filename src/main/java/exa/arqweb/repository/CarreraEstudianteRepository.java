b91654bf7da514178ea0e3470e721986eb202458
pedrochatelain <pedrochatelain@gmail.com>
2023-09-14 19:58:44 -0300

    implementa metodo para graduar

---
+++ src/main/java/exa/arqweb/repository/CarreraEstudianteRepository.java
@@ -0,0 +1,47 @@
+package exa.arqweb.repository;
+
+import exa.arqweb.entity.Carrera;
+import exa.arqweb.entity.Estudiante;
+import exa.arqweb.entity.CarreraEstudiante;
+import exa.arqweb.repository.interfaces.Repository;
+import exa.arqweb.utils.CarreraEstudianteKey;
+import jakarta.persistence.EntityManager;
+
+public class CarreraEstudianteRepository implements Repository<CarreraEstudiante> {
+
+    private EntityManager em;
+
+    public CarreraEstudianteRepository(EntityManager em) {
+        this.em = em;
+    }
+
+    // b) matricular un estudiante en una carrera
+    public void matricular(int id_estudiante, int id_carrera, int anio_inscripcion) {
+        em.getTransaction().begin();
+        Estudiante estudiante = em.find(Estudiante.class, id_estudiante);
+        Carrera carrera = em.find(Carrera.class, id_carrera);
+        CarreraEstudiante inscripcion = new CarreraEstudiante(estudiante, carrera, anio_inscripcion);
+        em.persist(inscripcion);
+        em.getTransaction().commit();
+    }
+
+    public void graduar(int id_estudiante, int id_carrera, int anio_graduacion) {
+        em.getTransaction().begin();
+        CarreraEstudianteKey key = new CarreraEstudianteKey(id_estudiante, id_carrera);
+        CarreraEstudiante carreraEstudiante = em.find(CarreraEstudiante.class, key);
+        if (anio_graduacion >= carreraEstudiante.getAnioInscripcion()) {
+            carreraEstudiante.setAnioGraduacion(anio_graduacion);
+        }
+        em.getTransaction().commit();
+    }
+
+    @Override
+    public void delete(CarreraEstudiante ins) {
+    }
+
+    @Override
+    public void add(CarreraEstudiante p) {
+     
+    }
+    
+}
