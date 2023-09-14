1970b0ec5516e189a438e62533b0d60de07feadc
pedrochatelain <pedrochatelain@gmail.com>
2023-09-14 13:04:04 -0300

    borra metodos sin utilizar en la clase Estudiante

--- src/main/java/exa/arqweb/entity/Estudiante.java
+++ src/main/java/exa/arqweb/entity/Estudiante.java
@@ -60,84 +60,16 @@ public class Estudiante {
         this.inscripciones = new LinkedList<>();
     }
 
-    public int getID() {
-        return this.id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-
-    public int getEdad() {
-        return edad;
-    }
-
-    public void setEdad(int edad) {
-        this.edad = edad;
-    }
-
-    public String getGenero() {
-        return genero;
-    }
-
-    public void setGenero(String genero) {
-        this.genero = genero;
-    }
-
-    public int getDni() {
-        return dni;
-    }
-
-    public void setDni(int dni) {
-        this.dni = dni;
-    }
-
-    public String getCiudad() {
-        return ciudad;
-    }
-
-    public void setCiudad(String ciudad) {
-        this.ciudad = ciudad;
-    }
-
-    public int getLegajo() {
-        return legajo;
-    }
-
-    public void setLegajo(int legajo) {
-        this.legajo = legajo;
-    }
-
-    public boolean esGraduado() {
-        return esGraduado;
-    }
-
-    public void setGraduado(boolean esGraduado) {
-        this.esGraduado = esGraduado;
-    }
-
-    public void addInscripcion(Inscripcion i) {
-        this.inscripciones.add(i);
-    }
-
     @Override
     public String toString() {
         return "\nID: " + id + "\nNombre: " + nombre + "\nApellido: " + apellido
         + "\nEdad: " + edad + "\nGénero: " + genero + "\nDNI: "
         + dni + "\nCiudad: " + ciudad + "\nLegajo: " + legajo +
         "\nEs graduado: " + esGraduado;
+    }
+
+    public int getID() {
+        return this.id;
     }
 
 }
